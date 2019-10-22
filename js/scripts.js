55d4f9f96c03d5df45aa0d6a922ee8158598bcec
Student <example@example.com>
2019-10-22 15:19:01 -0700

    add text / definitions & fix toggles

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,86 @@
 $(document).ready(function() {
-   $(".card").click(function flip() {
-    $('.front').toggle();
-    $('.back').toggle();
+   $("#welcome").click(function flip() {
+    $('#welcome .front').toggle();
+    $('#welcome .back').toggle();
+  });
+   $("#operators").click(function flip() {
+    $('#operators .front').toggle();
+    $('#operators .back').toggle();
+  });
+   $("#javascript").click(function flip() {
+    $('#javascript .front').toggle();
+    $('#javascript .back').toggle();
+  });
+   $("#variables").click(function flip() {
+    $('#variables .front').toggle();
+    $('#variables .back').toggle();
+  });
+   $("#variable-naming").click(function flip() {
+    $('#variable-naming .front').toggle();
+    $('#variable-naming .back').toggle();
+  });
+   $("#methods").click(function flip() {
+    $('#methods .front').toggle();
+    $('#methods .back').toggle();
+  });
+   $("#functions").click(function flip() {
+    $('#functions .front').toggle();
+    $('#functions .back').toggle();
+  });
+   $("#arguments").click(function flip() {
+    $('#arguments .front').toggle();
+    $('#arguments .back').toggle();
+  });
+   $("#parameters").click(function flip() {
+    $('#parameters .front').toggle();
+    $('#parameters .back').toggle();
+  });
+   $("#return").click(function flip() {
+    $('#return .front').toggle();
+    $('#return .back').toggle();
+  });
+   $("#row").click(function flip() {
+    $('#row .front').toggle();
+    $('#row .back').toggle();
+  });
+   $("#strings").click(function flip() {
+    $('#strings .front').toggle();
+    $('#strings .back').toggle();
+  });
+   $("#booleans").click(function flip() {
+    $('#booleans .front').toggle();
+    $('#booleans .back').toggle();
+  });
+   $("#undefined").click(function flip() {
+    $('#undefined .front').toggle();
+    $('#undefined .back').toggle();
+  });
+   $("#nan").click(function flip() {
+    $('#nan .front').toggle();
+    $('#nan .back').toggle();
+  });
+   $("#escape").click(function flip() {
+    $('#escape .front').toggle();
+    $('#escape .back').toggle();
+  });
+   $("#alert").click(function flip() {
+    $('#alert .front').toggle();
+    $('#alert .back').toggle();
+  });
+   $("#comments").click(function flip() {
+    $('#comments .front').toggle();
+    $('#comments .back').toggle();
+  });
+   $("#jquery").click(function flip() {
+    $('#jquery .front').toggle();
+    $('#jquery .back').toggle();
+  });
+   $("#attributes").click(function flip() {
+    $('#attributes .front').toggle();
+    $('#attributes .back').toggle();
+  });
+   $("#chaining").click(function flip() {
+    $('#chaining .front').toggle();
+    $('#chaining .back').toggle();
   });
 })
